b16779387bf7108c48762e2f4a34add9eb8e159a
haja mydeen <120375547+haja2611@users.noreply.github.com>
2024-03-26 15:14:06 +0530

    Production ReadyAI changed

--- src/Components/HomePage/Section/02_ProductionReadyAI/ProductionReadyAI.jsx
+++ src/Components/HomePage/Section/02_ProductionReadyAI/ProductionReadyAI.jsx
@@ -6,25 +6,23 @@ const ProductionReadyAi = () => {
     console.log("Button clicked!");
   };
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-12 gap-3 bg-blue-100">
+    <div className="grid grid-cols-1 lg:grid-cols-12 gap-3 py-3 bg-[#e6f0f2]">
       {/* Top Grid (lg 12) */}
       <div className="col-span-12 lg:col-span-full mt-6">
-        <p className="font-sans text-4xl lg:text-6xl font-normal text-left pl-10 pt-10 lg:ml-8 text-black">
-          <span className="font-bold text-purple-900">
-            Production-ready AI,
-          </span>
+        <p className="font-sans text-4xl md:text-[44px] md:leading-[52.8px] leading-9 font-normal md:font-bold text-left pl-10 pt-10 md:ml-6 text-black">
+          <span className=" text-[#340D73]">Production-ready AI,</span>
           <br /> enriched with industry experience
         </p>
       </div>
-      <div className="col-span-12 sm:col-span-full md:col-span-full lg:col-span-6 mt-8">
-        <p className="bg-purple-900 text-white font-sans text-2xl lg:text-3xl font-light text-left p-10 rounded-lg ml-8">
+      <div className="col-span-12 sm:col-span-full md:col-span-full lg:col-span-6 mx-6 md:mx-0 md:ml-6 mt-8">
+        <p className="bg-[#340D73] text-white font-sans text-xl md:text-[27px] md:leading-[40.5px] leading-7  font-light text-left p-10 rounded-lg ml-8">
           Our market ready solutions provide flexible and scalable capabilities
           that enterprises need to successfully become AI-fueled organizations
         </p>
       </div>
       {/* Bottom Grids (lg 6) */}
-      <div className="col-span-12 sm:col-span-full md:col-span-full lg:col-span-6 mt-8">
-        <p className="text-gray-700 font-sans font-medium text-lg lg:text-xl leading-9 text-left pl-10 pb-5 lg:ml-20">
+      <div className="col-span-12 sm:col-span-full md:col-span-full lg:col-span-6 mx-6 md:mx-0 mt-8">
+        <p className="text-[#57647c] font-sans font-normal text-xl leading-[30px] text-left pl-10 md:pr-5 pb-5 ">
           Redefining the infusion of machine learning in heavy industries for a
           decade, we at Shodat Inc are poised for a paradigm shift in the
           industry with ready AI—eOps Fabric™, an edge analytics, and operations
@@ -33,7 +31,7 @@ const ProductionReadyAi = () => {
           defects, automate RCA reports, and propose impactful enhancements that
           promise significant cost benefits.
         </p>
-        <div className="py-10 md:ml-[100px]">
+        <div className="py-5 ml-9">
           <button
             className=" text-black font-openSans text-base font-normal py-2 px-4 h-11  w-40 rounded-full flex items-center justify-center"
             style={{
